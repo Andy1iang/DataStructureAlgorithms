7023e1bf30b7f43384f0b132e01f901d07d3b1cc
Andy1iang <andyliang254@gmail.com>
2024-02-18 21:10:20 -0500

    Updated DLL Class

--- DLL.py
+++ DLL.py
@@ -1,101 +1,183 @@
 class Node:
 
-	def __init__(self, data):
-		self.data = data
-		self.prev = None
-		self.next = None
+    def __init__(self, data):
+        self.data = data
+        self.prev = None
+        self.next = None
 
-	def __str__(self):
-		return str(self.data)
+    def __str__(self):
+        return self.data
 
 
 class DLL:
 
-	def __init__(self):
-		self.head = None
-		self.tail = None
-		self.count = 0
+    def __init__(self):
 
-	def add_head(self, data):
-		new_node = Node(data)
-		self.count += 1
-		if self.head is None:
-			# if DLL is empty, instantiate head and tail node (same node)
-			self.head = new_node
-			self.tail = new_node
-		else:
-			new_node.next = self.head
-			self.head.prev = new_node
-			self.head = new_node
+        self.head = None
+        self.tail = None
+        self.size = 0
 
-	def add_tail(self, data):
-		new_node = Node(data)
-		self.count += 1
-		if self.tail is None:
-			# if DLL is empty, instantiate head and tail node (same node)
-			self.head = new_node
-			self.tail = new_node
-		else:
-			new_node.prev = self.tail
-			self.tail.next = new_node
-			self.tail = new_node
+    def add_head(self, data):
 
-	def traverse_forward(self):
-		temp = self.head
-		while temp is not None:
-			print(temp)
-			temp = temp.next
+        new_node = Node(data)
 
-	def traverse_backward(self):
-		temp = self.tail
-		while temp is not None:
-			print(temp)
-			temp = temp.prev
+        if self.size == 0:
+            # if DLL is empty, instantiate head and tail node (same node)
+            self.head = new_node
+            self.tail = new_node
 
-	def reverse(self):
-		curr_node = self.head
-		temp = None
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
 
-		while curr_node is not None:
-			temp = curr_node.prev
-			curr_node.prev = curr_node.next
-			curr_node.next = temp
-			curr_node = curr_node.prev
+        self.size += 1
 
-		self.head = temp.prev
+    def add_tail(self, data):
 
-	def remove(self, target):
-		# O(N) time complexity
-		if self.head is None:
-			return None
-		else:
-			temp = self.head
-			while temp is not None and temp.data != target:
-				temp = temp.next
+        new_node = Node(data)
 
-			if temp is None:
-				return None
+        if self.size == 0:
+            # if DLL is empty, instantiate head and tail node (same node)
+            self.head = new_node
+            self.tail = new_node
 
-			elif self.count == 1:
-				# if there's only one node, reset DLL by deleting that node
-				self.head = None
-				self.tail = None
+        else:
+            new_node.prev = self.tail
+            self.tail.next = new_node
+            self.tail = new_node
 
-			elif temp.prev is None:
-				# disconnect new head node with previous node
-				self.head = temp.next
-				self.head.prev = None
+        self.size += 1
 
-			elif temp.next is None:
-				# disconnect new tail node with previous node
-				self.tail = temp.prev
-				self.tail.next = None
-			else:
-				temp.prev.next = temp.next
-				temp.next.prev = temp.prev
+    def insert(self, data):
 
-			self.count -= 1
-			return temp
+        # given that the DLL is ordered
 
-	def node_count(self):
-		return self.count
+        if self.size == 0:
+            self.add_head(data)
+
+        elif data < self.head.data:
+            self.add_head(data)
+
+        elif data > self.tail.data:
+            self.add_tail(data)
+
+        else:
+            temp = self.head
+            while temp is not None and temp.data < data:
+                temp = temp.next
+
+            new_node = Node(data)
+            new_node.next = temp
+            new_node.prev = temp.prev
+            temp.prev.next = new_node
+            temp.prev = new_node
+            self.size += 1
+
+    def remove_head(self):
+
+        if self.size == 0:
+            return None
+
+        elif self.size == 1:
+            temp = self.head
+            self.head = None
+            self.tail = None
+            self.size -= 1
+            return temp
+
+        else:
+            temp = self.head
+            self.head = self.head.next
+            self.head.prev = None
+            self.size -= 1
+            return temp
+
+    def remove_tail(self):
+
+        if self.size == 0:
+            return None
+
+        elif self.size == 1:
+            temp = self.tail
+            self.tail = None
+            self.head = None
+            self.size -= 1
+            return temp
+
+        else:
+            temp = self.tail
+            self.tail = self.tail.prev
+            self.tail.next = None
+            self.size -= 1
+            return temp
+
+    def remove(self, target):
+
+        if self.size == 0:
+            return None
+
+        elif target == self.head.data:
+            return self.remove_head()
+
+        elif target == self.tail.data:
+            return self.remove_tail()
+
+        else:
+            temp = self.head
+            while temp is not None and temp.data != target:
+                temp = temp.next
+
+            if temp is None:
+                return None
+
+            else:
+                temp.prev.next = temp.next
+                temp.next.prev = temp.prev
+                self.size -= 1
+                return temp
+
+    def traverse_forward(self):
+
+        temp = self.head
+        while temp is not None:
+            print(temp)
+            temp = temp.next
+
+    def traverse_backward(self):
+
+        temp = self.tail
+        while temp is not None:
+            print(temp)
+            temp = temp.prev
+
+    def reverse(self):
+
+        if self.size > 1:
+
+            prev = None
+            curr = self.head
+            self.tail = self.head
+
+            while curr is not None:
+                curr.next, curr.prev = curr.prev, curr.next
+                prev = curr
+                curr = curr.prev
+
+            self.head = prev
+
+    def __len__(self):
+        return self.size
+
+    def __repr__(self):
+        res = []
+        res.append(f'DLL With {self.size} Nodes, stored at {hex(id(self))}')
+
+        temp = self.head
+        idx = 1
+        while temp is not None:
+            res.append(f'Node {idx}: {str(temp)}')
+            temp = temp.next
+            idx += 1
+
+        return '\n'.join(res)
